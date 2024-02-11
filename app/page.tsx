a2b7f3bc86964ea1a74b015386eb8f2904da0c59
Prashant Aghara <prashant.aghara2111@gmail.com>
2024-02-11 16:56:27 +0530

    Added Live Cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+import Live from "@/components/Live";
 import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="font-2xl text-white">Figma</h1>
+    <div>
+      <Live />
     </div>
   );
 }
